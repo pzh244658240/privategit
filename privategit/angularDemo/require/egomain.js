de2ad7250da9cfcc2f35648a8bfbb8785f721500
pengzhonghui <pengzhonghui@pengzhonghui-PC>
2017-05-11 17:57:46 +0800

    添加requirejs的用例

---
+++ privategit/angularDemo/require/egomain.js
@@ -0,0 +1,58 @@
+requirejs.config({
+    baseUrl: basePath+'/resources/dist/',
+    paths: {
+    	  jquery : basePath+'/resources/library/bower_components/jquery/dist/jquery.min',
+    	  bootstrap : basePath+'/resources/library/bower_components/bootstrap/dist/js/bootstrap.min',
+    	  lhgcalendar : basePath+'/resources/library/bower_components/privategit/privategit/lib/lhgcalender/lhgcalendar.min',
+    	  stats : basePath+'/resources/library/bower_components/stats.js/build/stats.min',
+    	  respond : basePath+'/resources/library/bower_components/respond/dest/respond.min',
+    	  jqueryserialize : basePath+'/resources/library/bower_components/jqueryserialize/dist/jquery.serialize-object.min',
+    	  jsonutil : basePath+'/resources/library/bower_components/privategit/privategit/lib/JSON',
+    	  angular : basePath+'/resources/library/bower_components/angular/angular.min',
+    	  angularRoute : basePath+'/resources/library/bower_components/angularjs-route/angular-route.min',
+    	  angularUiRouter : basePath+'/resources/library/bower_components/angular-ui-router/release/angular-ui-router.min',
+    	  ocLazyLoad : basePath+'/resources/library/bower_components/oclazyload/dist/ocLazyLoad.min',
+    	  map : basePath+'/resources/library/bower_components/privategit/privategit/lib/map',
+    	  popupWindow : basePath+'/resources/library/common/popupWindow',
+    	  ace : basePath+'/resources/library/bower_components/ace/ace'
+    },
+    shim: {
+        angular : { exports : 'angular'},
+        jquery:{exports:'jquery'},
+        bootstrap:{exports:'bootstrap'},
+        lhgcalendar:{exports:'lhgcalendar'},
+        stats:{exports:'stats'},
+        respond:{exports:'respond'},
+        jqueryserialize:{exports:'jqueryserialize'},
+        jsonutil:{exports:'jsonutil'},
+        map:{exports:'map'},
+        "angular-route":{exports:"angular-route"},
+        "angular-ui-router":{exports:"angular-ui-router"},
+        ocLazyLoad:{exports:"ocLazyLoad"},
+        formjs:{exports:"formjs"},
+       // colResizable:{ exports : 'colResizable'}
+    }
+}); 
+var pathname = location.pathname;
+var timestamp=new Date().getTime();
+var projectpath = basePath.substring(1,basePath.length)+"/portal";
+requirejs([ 'jquery','angular','bootstrap','map','popupWindow','lhgcalendar'],
+function ($,b,canvas,mp) {
+	requirejs([basePath+'/resources/dist/js/portal/dataview.all.js?ver='+version], function (d) {
+
+	});
+});
+
+//require("js/portal/project/controller/baseControl");
+
+/*// Start the main app logic.,'js/common/dev/statemain'
+requirejs(['bootstrap', 'map', 'js/portal/project/projectinfo'],
+function ($,b,canvas,sub) {
+   
+});
+requirejs(['bootstrap', 'map', 'js/common/popupWindow'],
+		function ($,b,canvas,sub) {
+	inintWindowPopup('differenceWin', 'differenceTargetWin', true, true);
+    inintWindowPopup('spanWin', 'spanTargetWin', true, true);
+    inintWindowPopup('transportWin', 'transportTargetWin', true, true);
+});*/
